0f07d92d5e96a257a6c0c71493776ab2a1d04d83
real_issa <isanur0202@gmail.com>
2025-02-20 00:56:04 +0500

    model constructors added

--- src/main/java/org/epam/model/Trainee.java
+++ src/main/java/org/epam/model/Trainee.java
@@ -1,12 +1,21 @@
 package org.epam.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.Date;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class Trainee extends User {
     private Date dateOfBirth;
     private String address;
     private Long userId;
+
+    public Trainee(Long userId, String firstName, String lastName, String username, String password, boolean active, Date dateOfBirth, String address) {
+        super(firstName, lastName, username, password, active);
+        this.userId = userId;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
 }
--- src/main/java/org/epam/model/User.java
+++ src/main/java/org/epam/model/User.java
@@ -1,10 +1,12 @@
 package org.epam.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 abstract public class User {
     private String firstName;
     private String lastName;
