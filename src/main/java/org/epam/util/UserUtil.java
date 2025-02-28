75721310d7c0fadcbe950d7eb280989218a64815
real_issa <isanur0202@gmail.com>
2025-02-28 15:03:15 +0500

    Userutil to generate username and password

---
+++ src/main/java/org/epam/util/UserUtil.java
@@ -0,0 +1,29 @@
+package org.epam.util;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class UserUtil {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final Random RANDOM = new SecureRandom();
+
+    public static String generateUsername(String firstName, String lastName, java.util.function.Predicate<String> usernameExists) {
+        String baseUsername = firstName + "." + lastName;
+        String username = baseUsername;
+        int counter = 1;
+
+        while (usernameExists.test(username)) {
+            username = baseUsername + counter;
+            counter++;
+        }
+        return username;
+    }
+
+    public static String generatePassword() {
+        StringBuilder password = new StringBuilder(10);
+        for (int i = 0; i < 10; i++) {
+            password.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+        }
+        return password.toString();
+    }
+}
