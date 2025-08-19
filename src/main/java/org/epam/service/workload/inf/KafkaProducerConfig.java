81f26a5fe70d23d2d7b4036e22ad6bc1ffe9190d
real_issa <isanur0202@gmail.com>
2025-08-19 07:47:03 +0500

    done

--- src/main/java/org/epam/service/workload/inf/KafkaProducerConfig.java
+++ src/main/java/org/epam/service/workload/inf/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package org.epam.service.workload.inf;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.epam.service.workload.dto.UpdateReport;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
@@ -13,11 +14,13 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public ProducerFactory<String, UpdateReport> producerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 org.springframework.kafka.support.serializer.JsonSerializer.class);
